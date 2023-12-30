328218b1dd1065c40b04cadbca45236face47f28
swlman <duranalejandro661@gmail.com>
2023-12-30 20:52:09 -0300

    [STYLES] Adjusting font-sizes on each breakpoint. Adjusting their correspondig padding and margin

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const re =
+const emailSyntax =
   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 const emailInput = document.getElementById("email-input");
 const suscribe = document.getElementById("suscribe-button");
-const errorMessage = document.getElementsByClassName("error-message hidden")[0];
+const errorMessage = document.getElementById("error-message");
 
 suscribe.setAttribute("disabled", "");
 
@@ -17,20 +17,19 @@ suscribe.addEventListener("click", (event) => {
   window.location.href = "./success.html"
 });
 
-
-
-const toggleButton = () => {
+const enableSubmit = () => {
   emailInput.classList.remove("error");
   errorMessage.classList.add("hidden");
 };
 
 const validateEmail = (email) => {
-  if (String(email).match(re)) {
-    toggleButton();
+  if (String(email).match(emailSyntax)) {
+    enableSubmit();
     suscribe.removeAttribute("disabled");
   } else if (email.length <= 0) {
-    toggleButton();
+    enableSubmit();
   } else {
+    /*Email Validation Failed*/
     emailInput.classList.add("error");
     errorMessage.classList.remove("hidden");
   }
